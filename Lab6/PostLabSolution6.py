677e4018e5fd7220c46a2e6f8467e83bc490286a
jvocortez <jvocortez@mymail.mapua.edu.ph>
2025-07-25 18:08:47 +0800

    Update PostLabSolution6.py

--- Lab6/PostLabSolution6.py
+++ Lab6/PostLabSolution6.py
@@ -1,1 +1,26 @@
+db = connect("localhost:27017/musicDatabase");
 
+db.artists.insertOne({
+  ArtistId: 1,
+  Name: "Sample Artist",
+  Albums: [1]
+});
+
+db.albums.insertOne({
+  AlbumId: 1,
+  Title: "Sample Album",
+  ArtistId: 1,
+  Tracks: [1]
+});
+
+db.tracks.insertOne({
+  TrackId: 1,
+  Name: "Sample Track",
+  AlbumId: 1,
+  MediaTypeId: 1,
+  GenreId: 1,
+  Composer: "Sample Composer",
+  Milliseconds: 250000,
+  Bytes: 1024000,
+  UnitPrice: 0.99
+});
